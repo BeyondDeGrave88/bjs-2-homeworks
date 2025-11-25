fdba365df138bfb1a758e2796444f7e680554a0c
Dmitrij Sokolov <darkfalken88@gmail.com>
2025-11-25 14:40:31 +0300

    Добавлено решение для 3.arrays/task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
+//1
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+//2
+function getUsersNamesInAgeRange(people, gender) {
+	const result = people
+		.filter(people => people.gender === gender)
+		.map(people => people.age)
+		.reduce((average, age, index, array) => {
+			return average + age / array.length;
+		}, 0);
+
+	return result;
 }
